1b6e2e742e33a7f92b0a4eb398614833cb378998
yair8 <yair.shemer1@mail.huji.ac.il>
2023-04-16 16:43:08 +0300

    Revert "try init weights to not be zero"
    This reverts commit df6b65db56af5ccf57dee3bf76c7cd070b3b1b90.

--- encodec/modules/seanet.py
+++ encodec/modules/seanet.py
@@ -142,13 +142,6 @@ class SEANetEncoder(nn.Module):
         ]
 
         self.model = nn.Sequential(*model)
-        self.model.apply(self._init_weights)
-
-    @staticmethod
-    def _init_weights(m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform(m.weight)
-            m.bias.data.fill_(0.01)
 
     def forward(self, x):
         return self.model(x)
@@ -243,13 +236,6 @@ class SEANetDecoder(nn.Module):
                 final_act(**final_activation_params)
             ]
         self.model = nn.Sequential(*model)
-        self.model.apply(self._init_weights)
-
-    @staticmethod
-    def _init_weights(m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform(m.weight)
-            m.bias.data.fill_(0.01)
 
     def forward(self, z):
         y = self.model(z)

a0f18bb614b59ab81c2d1a63ed92f167a576c666
yair8 <yair.shemer1@mail.huji.ac.il>
2023-05-29 12:31:11 +0300

    take necessary funcs from orig distrib

--- encodec/distrib.py
+++ encodec/distrib.py
@@ -7,15 +7,31 @@
 
 import logging
 import os
-
+import typing as tp
 import torch
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader, Subset
 from torch.nn.parallel.distributed import DistributedDataParallel
 
 logger = logging.getLogger(__name__)
-rank = 0
-world_size = 1
+
+
+def rank():
+    if torch.distributed.is_initialized():
+        return torch.distributed.get_rank()
+    else:
+        return 0
+
+
+def is_distributed():
+    return world_size() > 1
+
+
+def world_size():
+    if torch.distributed.is_initialized():
+        return torch.distributed.get_world_size()
+    else:
+        return 1
 
 
 def init(args):
@@ -23,20 +39,19 @@ def init(args):
 
     Initialize DDP using the given rendezvous file.
     """
-    global rank, world_size
-    if args.ddp:
-        assert args.rank is not None and args.world_size is not None
-        rank = args.rank
-        world_size = args.world_size
-    if world_size == 1:
+    # if args.ddp:
+    #     assert args.rank is not None and args.world_size is not None
+    #     rank = args.rank
+    #     world_size = args.world_size
+    if world_size() == 1:
         return
-    torch.cuda.set_device(rank)
+    torch.cuda.set_device(rank())
     torch.distributed.init_process_group(
         backend=args.ddp_backend,
         init_method='file://' + os.path.abspath(args.rendezvous_file),
-        world_size=world_size,
-        rank=rank)
-    logger.debug("Distributed rendezvous went well, rank %d/%d", rank, world_size)
+        world_size=world_size(),
+        rank=rank())
+    logger.debug("Distributed rendezvous went well, rank %d/%d", rank(), world_size())
 
 
 def average(metrics, count=1.):
@@ -46,7 +61,7 @@ def average(metrics, count=1.):
     `metrics`should be a 1D float32 vector. Returns the average of `metrics`
     over all hosts. You can use `count` to control the weight of each worker.
     """
-    if world_size == 1:
+    if world_size() == 1:
         return metrics
     tensor = torch.tensor(list(metrics) + [1], device='cuda', dtype=torch.float32)
     tensor *= count
@@ -54,12 +69,32 @@ def average(metrics, count=1.):
     return (tensor[:-1] / tensor[-1]).cpu().numpy().tolist()
 
 
+def average_metrics(metrics: tp.Dict[str, float], count=1.):
+    """Average a dictionary of metrics across all workers, using the optional
+    `count` as unnormalized weight.
+    """
+    if not is_distributed():
+        return metrics
+    keys, values = zip(*metrics.items())
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    tensor = torch.tensor(list(values) + [1], device=device, dtype=torch.float32)
+    tensor *= count
+    all_reduce(tensor)
+    averaged = (tensor[:-1] / tensor[-1]).cpu().tolist()
+    return dict(zip(keys, averaged))
+
+
+def all_reduce(tensor: torch.Tensor, op=torch.distributed.ReduceOp.SUM):
+    if is_distributed():
+        return torch.distributed.all_reduce(tensor, op)
+
+
 def wrap(model):
     """wrap.
 
     Wrap a model with DDP if distributed training is enabled.
     """
-    if world_size == 1:
+    if world_size() == 1:
         return model
     else:
         return DistributedDataParallel(
@@ -69,7 +104,7 @@ def wrap(model):
 
 
 def barrier():
-    if world_size > 1:
+    if world_size() > 1:
         torch.distributed.barrier()
 
 
@@ -86,7 +121,7 @@ def loader(dataset, *args, shuffle=False, klass=DataLoader, **kwargs):
     :param kwargs: relevant args
     """
 
-    if world_size == 1:
+    if world_size() == 1:
         return klass(dataset, *args, shuffle=shuffle, **kwargs)
 
     if shuffle:
@@ -96,5 +131,80 @@ def loader(dataset, *args, shuffle=False, klass=DataLoader, **kwargs):
         return klass(dataset, *args, **kwargs, sampler=sampler)
     else:
         # We make a manual shard, as DistributedSampler otherwise replicate some examples
-        dataset = Subset(dataset, list(range(rank, len(dataset), world_size)))
+        dataset = Subset(dataset, list(range(rank(), len(dataset), world_size())))
         return klass(dataset, *args, shuffle=shuffle)
+
+
+def _is_complex_or_float(tensor):
+    return torch.is_floating_point(tensor) or torch.is_complex(tensor)
+
+
+def _check_number_of_params(params: tp.List[torch.Tensor]):
+    # utility function to check that the number of params in all workers is the same,
+    # and thus avoid a deadlock with distributed all reduce.
+    if not is_distributed() or not params:
+        return
+    tensor = torch.tensor([len(params)], device=params[0].device, dtype=torch.long)
+    all_reduce(tensor)
+    if tensor.item() != len(params) * world_size():
+        # If not all the workers have the same number, for at least one of them,
+        # this inequality will be verified.
+        raise RuntimeError(f"Mismatch in number of params: ours is {len(params)}, "
+                           "at least one worker has a different one.")
+
+
+def broadcast_tensors(tensors: tp.Iterable[torch.Tensor], src: int = 0):
+    """Broadcast the tensors from the given parameters to all workers.
+    This can be used to ensure that all workers have the same model to start with.
+    """
+    if not is_distributed():
+        return
+    tensors = [tensor for tensor in tensors if _is_complex_or_float(tensor)]
+    _check_number_of_params(tensors)
+    handles = []
+    for tensor in tensors:
+        handle = torch.distributed.broadcast(tensor.data, src=src, async_op=True)
+        handles.append(handle)
+    for handle in handles:
+        handle.wait()
+
+
+def sync_buffer(buffers, average=True):
+    """
+    Sync grad for buffers. If average is False, broadcast instead of averaging.
+    """
+    if not is_distributed():
+        return
+    handles = []
+    for buffer in buffers:
+        if torch.is_floating_point(buffer.data):
+            if average:
+                handle = torch.distributed.all_reduce(
+                    buffer.data, op=torch.distributed.ReduceOp.SUM, async_op=True)
+            else:
+                handle = torch.distributed.broadcast(
+                    buffer.data, src=0, async_op=True)
+            handles.append((buffer, handle))
+    for buffer, handle in handles:
+        handle.wait()
+        if average:
+            buffer.data /= world_size
+
+
+def sync_grad(params):
+    """
+    Simpler alternative to DistributedDataParallel, that doesn't rely
+    on any black magic. For simple models it can also be as fast.
+    Just call this on your model parameters after the call to backward!
+    """
+    if not is_distributed():
+        return
+    handles = []
+    for p in params:
+        if p.grad is not None:
+            handle = torch.distributed.all_reduce(
+                p.grad.data, op=torch.distributed.ReduceOp.SUM, async_op=True)
+            handles.append((p, handle))
+    for p, handle in handles:
+        handle.wait()
+        p.grad.data /= world_size()
